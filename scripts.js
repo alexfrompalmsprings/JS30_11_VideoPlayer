b19053567c87d58eba215f971363ca8ae1c2fc33
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-03-26 17:56:54 -0700

    create togglePlay function

--- scripts.js
+++ scripts.js
@@ -2,8 +2,26 @@
 
 //first is the player
 const player = document.querySelector('.player');
-console.log('hi i am the player')
+// console.log('hi i am the player')
 
 //the video feed we are going to be using for the video player
 const video = player.querySelector('.viewer');
-console.log(video);
+// console.log(video);
+
+// additional functions
+let progress = player.querySelector('.progress');
+let progressBar = player.querySelector('.progress__filled');
+let toggle = player.querySelector('.toggle');
+let skipButtons = player.querySelector('[data-skip');
+let ranges = player.querySelector('.player__slider');
+
+//toggle function
+function togglePlay(){
+
+  //if the video is paused
+  if(video.paused){
+    video.play()
+  } else{
+    video.pause();
+  }
+}
