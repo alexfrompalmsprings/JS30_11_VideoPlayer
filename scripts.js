c51ab21d8b5c19ffd4fc04a047e50c1bd6f25020
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-03-26 17:36:22 -0700

    import the video

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-console.log('hey i am the script.js')
+//Get the elements in the page
+
+//first is the player
+const player = document.querySelector('.player');
+console.log('hi i am the player')
+
+//the video feed we are going to be using for the video player
+const video = player.querySelector('.viewer');
+console.log(video);
